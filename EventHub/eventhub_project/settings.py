4bf66c6fa49f4f01f8997e0f342793fbf569649f
jay16y <165669412+jay16y@users.noreply.github.com>
2026-06-05 21:22:45 +0530

    update settings and urls for deployment

--- EventHub/eventhub_project/settings.py
+++ EventHub/eventhub_project/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'eventhub_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # For file uploads
 AUTH_USER_MODEL = 'auth.User'
+
+# Debug mode
+DEBUG = True
