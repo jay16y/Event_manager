c93f2f09ecbf1619a733aa8f2b76bb0dca115dd2
jay16y <165669412+jay16y@users.noreply.github.com>
2026-06-07 14:54:35 +0530

    Add allowed hosts and CSRF trusted origins for local development

--- EventHub/eventhub_project/settings.py
+++ EventHub/eventhub_project/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-3-yr7dcpll=c&^@di(o)$_tf=^e^nvy^0-23i10qfexfhv6x4c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+]
 
 
 # Application definition
